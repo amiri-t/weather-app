c9c2956c77f53a9849993b0454f665f23a6b01ea
Amir Tahiri <tahiriamir49@gmail.com>
2023-10-27 16:55:39 +0200

    File & Folder Structure and Dependencies
    I rearranged the file and folder structure, deleted some unnecessary files and added new one. I also installed some important dependencies.

---
+++ src/components/WeatherDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const WeatherDisplay = () => {
+  return <Container>WeatherDisplay</Container>;
+};
+const Container = styled.div``;
+
+export default WeatherDisplay;
